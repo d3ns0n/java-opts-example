51f13f54b8ad7552fc76fe2cae2dc8e929a7524f
D3ns0n <d3ns0n@d3ns0n.de>
2020-09-22 22:10:29 +0200

    improve wording

--- src/de/d3ns0n/Main.java
+++ src/de/d3ns0n/Main.java
@@ -3,7 +3,7 @@ package de.d3ns0n;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\n" + "Java app started");
+        System.out.println("\n" + "Found system properties:");
 
         System.getProperties().keySet().stream()
                 .filter(key -> key.toString().startsWith("-MY_"))
